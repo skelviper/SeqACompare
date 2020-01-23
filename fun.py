3050b43d4bacaf285f3de114502e851bdbd812b1
skelviper <skelviper@hotmail.com>
2020-01-23 17:14:56 +0800

    完成了序列比对的部分

--- fun.py
+++ fun.py
@@ -1,3 +1,156 @@
 #序列比对的相关函数
 
-class
+class function:
+    @staticmethod
+    def reverseCompliment(seq):
+        ReverseDict = {"A":"T","C":"G","G":"C","T":"A"}
+        ReverseSeq = ""
+        for base in seq:
+            ReverseSeq = ReverseDict[base] + ReverseSeq
+        return ReverseSeq
+
+    @staticmethod
+    def seqAlign(seq1,seq2):
+        print(smithWaterman.water(seq1,seq2))
+    
+
+
+class smithWaterman:
+    
+    match_award = 1
+    mismatch_penalty = -2
+    N = -1
+    gap_penalty = -3
+    Extendgap_recalibration = 1 #延伸gap的罚分是-2，检测到上一个来自于上方向或者左方向，就给gap_penalty+1
+
+    @staticmethod
+    def alignScore(seq1,seq2):
+        score =0
+        for i,j in zip(seq1,seq2):
+            if(i == "N" or j == "N"):
+                score -=1
+            elif (i == "-" or j =="-"):
+                score -= 2
+            elif (i == j):
+                score +=1
+            else :
+                score -=2 #mismatch
+        return score
+
+    # 初始化打分表
+    @staticmethod
+    def zeros(shape):
+        retval = []
+        for x in range(shape[0]):
+            retval.append([])
+            for y in range(shape[1]):
+                retval[-1].append(0)
+        return retval
+
+    # 计算得分
+    @staticmethod
+    def match_score(alpha, beta):
+        if alpha == beta:
+            return smithWaterman.match_award
+        elif alpha == 'N' or beta == 'N':
+            return smithWaterman.N
+        elif alpha == '-' or beta == '-':
+            return smithWaterman.gap_penalty
+        else:
+            return smithWaterman.mismatch_penalty
+
+    @staticmethod
+    def water(seq1, seq2):
+        m, n = len(seq1), len(seq2)  # length of two sequences
+
+        # Generate DP table and traceback path pointer matrix
+        score = smithWaterman.zeros((m+1, n+1))      # the DP table
+        pointer = smithWaterman.zeros((m+1, n+1))    # to store the traceback path
+
+        max_score = 0        # initial maximum score in DP table
+
+        # Calculate DP table and mark pointers
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):#第一行第一列都是0，所以循环从1开始。
+                score_diagonal = score[i-1][j-1] + smithWaterman.match_score(seq1[i-1], seq2[j-1])
+                #填充的格子从1开始，填充格子的1对应序列的0，所以match_score的下表要减去1
+                score_up = score[i][j-1] + smithWaterman.gap_penalty
+                score_left = score[i-1][j] + smithWaterman.gap_penalty
+
+                if (pointer[i][j-1] == ( 2)):
+                    score_up = score[i][j-1] + smithWaterman.gap_penalty + smithWaterman.Extendgap_recalibration
+                if (pointer[i-1][j] == (1 )):
+                    score_left = score[i-1][j] + smithWaterman.gap_penalty + smithWaterman.Extendgap_recalibration
+
+                score[i][j] = max(0,score_left, score_up, score_diagonal)
+
+                if score[i][j] == 0:
+                    pointer[i][j] = 0 # 0 means end of the path
+                if score[i][j] == score_left:
+                    pointer[i][j] = 1 # 1 means trace up
+                if score[i][j] == score_up:
+                    pointer[i][j] = 2 # 2 means trace left
+                if score[i][j] == score_diagonal:
+                    pointer[i][j] = 3 # 3 means trace diagonal
+                if score[i][j] >= max_score:
+                    max_i = i
+                    max_j = j
+                    max_score = score[i][j]
+
+        align1, align2 = '', ''    # initial sequences
+
+        i,j = max_i,max_j    # indices of path starting point
+
+        #traceback, follow pointers
+        while pointer[i][j] != 0:
+            if pointer[i][j] == 3:
+                align1 += seq1[i-1]
+                align2 += seq2[j-1]
+                i -= 1
+                j -= 1
+            elif pointer[i][j] == 2:
+                align1 += '-'
+                align2 += seq2[j-1]
+                j -= 1
+            elif pointer[i][j] == 1:
+                align1 += seq1[i-1]
+                align2 += '-'
+                i -= 1
+        #[align1,align2,symbol,maxscore] 
+        answerlist= smithWaterman.finalize(align1, align2)
+        answerlist.append(max_score)
+        return answerlist
+
+    @staticmethod
+    def finalize(align1, align2):
+        align1 = align1[::-1]    #reverse sequence 1
+        align2 = align2[::-1]    #reverse sequence 2
+
+        i,j = 0,0
+
+        #输出结果: alignment 
+        symbol = ''
+        identity = 0
+        for i in range(0,len(align1)):
+            # if two AAs are the same, then output the letter
+            if (align1[i] == align2[i]):                
+                symbol += "*"
+            # if one of them is N
+            elif (align1[i] =="N" or align2[i]=="N"):
+                symbol += "N"
+            # if they are not identical and none of them is gap
+            elif (align1[i] != align2[i] and align1[i] != '-' and align2[i] != '-'): 
+                symbol += 'M'
+            #if one of them is a gap, output a -
+            elif (align1[i] == '-' or align2[i] == '-'):          
+                symbol += '-'
+
+        return[align1,align2,symbol]
+
+    @staticmethod
+    def printer(answerlist):
+        #answerlist: [align1,align2,symbol,maxscore]
+        print("seq1 " + answerlist[0])
+        print("seq2 " + answerlist[1])
+        print("symb " + answerlist[2])
+        print("Best Alignment score: " + str(answerlist[3]))
--- newFunc.py
+++ newFunc.py
@@ -1,81 +1,27 @@
-import tkinter
- 
-class Gui(object):
-    def __init__(self):
-        self.root = tkinter.Tk()
-        self.root.geometry("320x400+800+400")
-        self.root.title('自动发送邮件')
-        # 标签控件
-        # 邮件号
-        self.label_yj = tkinter.Label(master=self.root, text='邮件号:')
-        self.label_yj.grid(row=0, column=0)
-        # 间谍号
-        self.label_jd = tkinter.Label(master=self.root, text='间谍号:')
-        self.label_jd.grid(row=1, column=0)
-        # 目标群
-        self.label_mb = tkinter.Label(master=self.root, text='目标群:')
-        self.label_mb.grid(row=2, column=0)
-        # 授权码
-        self.label_sq = tkinter.Label(master=self.root, text='授权码:')
-        self.label_sq.grid(row=3, column=0)
-        # 邮件标题
-        self.label_bt = tkinter.Label(master=self.root, text='邮件标题:')
-        self.label_bt.grid(row=4, column=0)
-        # 邮件内容
-        self.label_nr = tkinter.Label(master=self.root, text='邮件内容:')
-        self.label_nr.grid(row=5, column=0)
-        # 发送记录
-        self.label = tkinter.Label(master=self.root, text='发送记录:')
-        self.label.grid(row=7, column=0)
- 
-        # 输入控件
-        # 邮件号
-        self.entry_yj = tkinter.Entry(master=self.root)
-        self.entry_yj.grid(row=0, column=1)
-        # 间谍号
-        self.entry_jd = tkinter.Entry(master=self.root)
-        self.entry_jd.grid(row=1, column=1)
-        # 目标群
-        self.entry_mb = tkinter.Entry(master=self.root)
-        self.entry_mb.grid(row=2, column=1)
-        # 授权码
-        self.entry_sq = tkinter.Entry(master=self.root)
-        self.entry_sq.grid(row=3, column=1)
-        # 邮件标题
-        self.entry_bt = tkinter.Entry(master=self.root)
-        self.entry_bt.grid(row=4, column=1)
-        # 邮件内容
-        self.entry_nr = tkinter.Entry(master=self.root)
-        self.entry_nr.grid(row=5, column=1)
- 
-        # 按钮控件
-        # 提交信息按钮
-        #self.button_tj = tkinter.Button(master=self.root, text='开始运行', command=)
-        #self.button_tj.grid(row=6, column=0)
-        # 停止按钮(command=self.root.quit  关闭窗口)
-        self.button_tz = tkinter.Button(master=self.root, text='停止发送', command=self.root.quit)
-        self.button_tz.grid(row=6, column=1)
-        # 清空内容(command=self.delete    清空当前窗口的所有内容)
-        self.button_tz = tkinter.Button(master=self.root, text='清空', command=self.delete)
-        self.button_tz.grid(row=6, column=2)
- 
-        # 列表框控件(输出结果)
-        self.listbox = tkinter.Listbox(master=self.root, width=45, height=10)
-        self.listbox.grid(rowspan=4, columnspan=4)
- 
-        self.root.mainloop()
- 
-    # 清空输入框
-    def delete(self):
-        self.entry_yj.delete(0, 'end')
-        self.entry_jd.delete(0, 'end')
-        self.entry_mb.delete(0, 'end')
-        self.entry_sq.delete(0, 'end')
-        self.entry_bt.delete(0, 'end')
-        #self.eneftry_nr.delete(0, 'end')
-        print("work")
-    
-    #zai 
+from fun import *
 
+seq1 = "ACGCNCTTTTCTCGCGTACCTTTACTAATAGAATGCAAAGACGTCCCCCG"
+seq2 = "CTCGCGTACCTTTACTAAGAGAATGCGNAGACGTCCCCCGGNGGACCGAT"
 
-Gui()
+#list = function.seqAlign(seq1,seq2)
+list = smithWaterman.water(seq1,seq2)
+#list [align1,align2,symbol,maxscore]
+seqExpand1 = ""
+seqExpand2 = ""
+sign = ""
+
+seqExpand1+="-"*seq2.find(list[1])
+seqExpand2+="-"*seq1.find(list[0])
+seqExpand1 += seq1
+seqExpand2 += seq2
+sign += "-"*(seq2.find(list[1])+seq1.find(list[0]))
+sign += list[2]
+
+maxLength = max(len(seqExpand1),len(seqExpand2))
+seqExpand1 +="-"*(maxLength-len(seqExpand1))
+seqExpand2 +="-"*(maxLength-len(seqExpand2))
+sign += "-"*(maxLength-len(sign))
+
+print(seqExpand1)
+print(seqExpand2)
+print(sign)
